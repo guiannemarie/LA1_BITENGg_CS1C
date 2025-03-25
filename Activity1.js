d73ffe487d3d17af3c32f44aa18836206e6dbc11
guiannemarie <guiannemarie9@gmail.com>
2025-03-25 23:08:52 +0800

    Activity1.js
    Variables Activity

---
+++ Activity1.js
@@ -0,0 +1,9 @@
+let name = "Guianne";
+let YouWannaBecomeInYourLife = "Software Engineer";
+let gender = "Female";
+let twitterHandle = "guiannemarie9@gmail.com";
+
+console.log (name);
+cpnsole.log (YouWannaBecomeInYourLife);
+cpnsole.log (gender);
+console.log (twitterHandle);
