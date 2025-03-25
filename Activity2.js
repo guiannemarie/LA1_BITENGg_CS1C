fac7292a5e7155444403053bf7c874e33f34cecf
guiannemarie <guiannemarie9@gmail.com>
2025-03-25 23:12:20 +0800

    Activity2.js
    Numbers Activity

---
+++ Activity2.js
@@ -0,0 +1,8 @@
+let firstFavNum = 23;
+let secondFavNum = 11;
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** secondFavNum);
