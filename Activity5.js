c0ac90ddc0808b699c144a7bf19db1382700c1d2
guiannemarie <guiannemarie9@gmail.com>
2025-03-25 23:29:03 +0800

    Create Activity5.js
    Strings

---
+++ Activity5.js
@@ -0,0 +1,7 @@
+let favActorFirstName = "Darren";
+let favActorLastName = "Espanto";
+let fullName = favActorFirstName  + " " + favActorLastName;
+let uppercase = fullName.topUpperCase();
+let message = 'My Favorite Actor is ${uppercase}, ';
+message += 'his best show is the hows of us' ;
+console.log(message);
